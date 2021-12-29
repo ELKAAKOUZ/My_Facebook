97c5fcf3f34ff826f9ae749850b2f0ad2526f0f2
mahfoz <mahmoudmahfoz2000@gmail.com>
2021-12-29 20:43:14 +0200

    start project

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My_Facebook</title>
+        <meta
+          name="description"
+          content="this is a simulation for facebook app"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </div>
+  );
+}
